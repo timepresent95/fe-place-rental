38f2f32f03618d810e4f21307051144e199e85dd
timepresent95 <timepresent95@gmail.com>
2024-04-13 15:06:11 +0900

    shardcn 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/6.shared/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const NotoSans = Noto_Sans({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          NotoSans.variable
+        )}>
+        {children}
+      </body>
     </html>
   );
 }
