202b2d7649437d309de3dc8e9d193f3925c24c5c
timepresent95 <timepresent95@gmail.com>
2024-04-16 02:06:55 +0900

    DatePicker 추가

---
+++ 6.shared/ui/shardcn/components/DatePicker.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { cn } from "@/6.shared/lib/tailwindMerge";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { CalendarIcon } from "lucide-react";
+import { useState } from "react";
+import dayjs from "@/6.shared/lib/dayjs";
+import { Calendar } from "../ui/calendar";
+
+function DatePicker() {
+  const [date, setDate] = useState<Date>();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}>
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? dayjs(date).format("YYYY-MM-DD") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+DatePicker.displayName = "DatePicker";
+
+export default DatePicker;
