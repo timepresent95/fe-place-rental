b0880e6826fd4e947d1d753d415061e9a92eac41
timepresent95 <timepresent95@gmail.com>
2024-04-18 03:30:01 +0900

    예약 완료 페이지 구현

---
+++ 2.pages/reservationSuccess/page.tsx
@@ -0,0 +1,26 @@
+import MainLogo from "@/6.shared/ui/Icon/MainLogo";
+import Link from "next/link";
+
+function ReservationSuccessPage() {
+  return (
+    <main className="px-4 container">
+      <div className="mt-40 flex flex-col items-center">
+        <div className="bg-indigo-700 w-44 p-2 flex justify-center rounded">
+          <MainLogo />
+        </div>
+        <p className="text-2xl font-bold mt-10">
+          대관 예약 신청이 완료되었습니다.
+        </p>
+        <Link
+          href="/reservation/list"
+          className="text-indigo-700 text-lg font-semibold mt-4">
+          예약 목록으로 가기
+        </Link>
+      </div>
+    </main>
+  );
+}
+
+ReservationSuccessPage.displayName = "ReservationSuccessPage";
+
+export default ReservationSuccessPage;
