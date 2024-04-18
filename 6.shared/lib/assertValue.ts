446b6191dc41f75dbb8f87d58485d7a79fdbef71
timepresent95 <timepresent95@gmail.com>
2024-04-18 22:53:05 +0900

    assertValue 함수 타입 체크 강화

--- 6.shared/lib/assertValue.ts
+++ 6.shared/lib/assertValue.ts
@@ -1,4 +1,7 @@
-export const assertValue = <T>(value: T, message: string) => {
+export const assertValue = <T>(
+  value: T | undefined | null,
+  message: string
+) => {
   if (value === undefined || value === null) {
     throw new Error(message);
   }
