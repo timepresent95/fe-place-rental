043238e169af00e44708f8dd3fe2f297cdcd3198
timepresent95 <timepresent95@gmail.com>
2024-04-20 18:26:48 +0900

    msw 브라우저 코드 삭제
    - node에서 모두 처리하도록 변경하여 미사용

--- app/template.tsx
+++
@@ -1,6 +0,0 @@
-import WaitMswSetupInBrowser from "@/1.app/msw/WaitMswSetupComponent";
-import { PropsWithChildren } from "react";
-
-export default function RootTemplate({ children }: PropsWithChildren) {
-  return <WaitMswSetupInBrowser>{children}</WaitMswSetupInBrowser>;
-}
