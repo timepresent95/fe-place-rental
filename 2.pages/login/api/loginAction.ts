63525700dc472710ad829f762933e17358d46bbb
timepresent95 <timepresent95@gmail.com>
2024-04-30 22:00:50 +0900

    login 후에 로그인 여부 표시 안되는 오류 해결

--- 2.pages/login/api/loginAction.ts
+++ 2.pages/login/api/loginAction.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { postLogin } from "@/5.entities/User/api";
@@ -14,6 +15,7 @@ export default async function loginAction(
   const result = await postLogin(signupBody);
 
   if (result.status === "success") {
+    revalidatePath("/admin", "layout");
     return redirect("/");
   }
 
