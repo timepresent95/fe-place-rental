ea3aded92337bd15f00d9b5a70d82bb43cd507c9
timepresent95 <timepresent95@gmail.com>
2024-05-02 17:48:18 +0900

    mock account 정보 생성 기능 구현

--- mock/model/accounts.ts
+++ mock/model/accounts.ts
@@ -1,7 +1,37 @@
-interface accounts {
+import { User, createUser } from "./users";
+import { ConfictError } from "../errors";
+
+interface Account {
   email: string;
   password: string;
   role: string;
-  user_id: string;
-  created_at: Date;
+  userId: string;
+  createdAt: Date;
+}
+
+const accounts = new Map<string, Account>();
+
+export function createAccount(
+  payload: Pick<Account, "password"> & Omit<User, "id" | "createdAt">
+) {
+  if (accounts.has(payload.email)) {
+    throw new ConfictError("이미 존재하는 email입니다.");
+  }
+
+  const user = createUser({
+    firstName: payload.firstName,
+    lastName: payload.lastName,
+    email: payload.email,
+    phone: payload.phone,
+  });
+
+  accounts.set(payload.email, {
+    email: user.email,
+    password: payload.password,
+    role: "user",
+    createdAt: user.createdAt,
+    userId: user.id,
+  });
+
+  return accounts.get(payload.email) as Account;
 }
