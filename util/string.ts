0c101c781104cab79d21f0698d642ecf31263167
timepresent95 <timepresent95@gmail.com>
2024-05-05 04:08:22 +0900

    string utility 함수 추가

--- util/string.ts
+++ util/string.ts
@@ -5,3 +5,15 @@ export function convertCamelToSnake(source: string) {
 export function convertCamelToKebab(source: string) {
   return source.replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
 }
+
+export function convertSnakeToCamel(source: string) {
+  return source.replace(/([-_][a-z])/g, (group) =>
+    group.toUpperCase().replace(/[-_]/, "")
+  );
+}
+
+export function convertKebabToCamel(source: string) {
+  return source.replace(/([-][a-z])/g, (group) =>
+    group.toUpperCase().replace(/[-]/, "")
+  );
+}
