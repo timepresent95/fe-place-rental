b7bc7d30c9faae48ff4cefe35397709bcc07fc1f
timepresent95 <timepresent95@gmail.com>
2024-05-04 22:14:31 +0900

    table, dialogTable component 기능 구현

---
+++ components/client/dialogTable.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  type Table as TableType,
+} from "@tanstack/react-table";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/ui/dialog";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/ui/table";
+
+type TableData = Record<string, any>;
+
+const DialogActionContext = createContext<((v: any) => void) | null>(null);
+const DialogValueContext = createContext<TableData | null>(null);
+
+function DialogProvider<T extends TableData>({ children }: PropsWithChildren) {
+  const [data, setData] = useState<T | null>(null);
+  const changeData = (v: T) => {
+    if (data === null) {
+      return setData({ ...v });
+    }
+    setData({ ...data, ...v });
+  };
+  return (
+    <DialogActionContext.Provider value={changeData}>
+      <DialogValueContext.Provider value={data}>
+        {children}
+      </DialogValueContext.Provider>
+    </DialogActionContext.Provider>
+  );
+}
+
+function useDialogAction<T extends TableData>(): (v: T) => void {
+  const action = useContext(DialogActionContext);
+  if (action === null) {
+    throw new Error("useCounterValue should be used within CounterProvider");
+  }
+  return action;
+}
+
+function useDialogValue(): TableData {
+  const value = useContext(DialogValueContext);
+  if (value === null) {
+    throw new Error("useCounterValue should be used within CounterProvider");
+  }
+  return value;
+}
+
+function DialogTableBody<TData extends TableData>({
+  table,
+}: {
+  table: TableType<TData>;
+}) {
+  const action = useDialogAction();
+  return (
+    <TableBody>
+      {table.getRowModel().rows?.length ? (
+        table.getRowModel().rows.map((row) => (
+          <DialogTrigger
+            key={row.id}
+            asChild
+            onClick={() => action(row.original)}>
+            <TableRow data-state={row.getIsSelected() && "selected"}>
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          </DialogTrigger>
+        ))
+      ) : (
+        <TableRow>
+          <TableCell
+            colSpan={table.getAllColumns().length}
+            className="h-24 text-center">
+            No results.
+          </TableCell>
+        </TableRow>
+      )}
+    </TableBody>
+  );
+}
+
+interface DialogTableProps<TData extends TableData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+function DialogTable<TData extends TableData, TValue>({
+  columns,
+  data,
+}: DialogTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <Table>
+      <TableHeader>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <TableHead key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </TableHead>
+            ))}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <Dialog>
+        <DialogProvider>
+          <DialogTableBody table={table} />
+          <DialogContent className="break-all">
+            <p>1</p>
+          </DialogContent>
+        </DialogProvider>
+      </Dialog>
+    </Table>
+  );
+}
+
+export { DialogTable };
---
+++ components/client/table.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/ui/table";
+
+type TableData = Record<string, any>;
+
+interface DialogTableProps<TData extends TableData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+function DialogTable<TData extends TableData, TValue>({
+  columns,
+  data,
+}: DialogTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <Table>
+      <TableHeader>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <TableHead key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </TableHead>
+            ))}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}>
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell
+              colSpan={table.getAllColumns().length}
+              className="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
+DialogTable.displayName = "DialogTable";
+
+export { DialogTable };
