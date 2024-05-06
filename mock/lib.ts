ccc4dcae6fca0a3398ef3f45f2ea9e1894e9e9b9
timepresent95 <timepresent95@gmail.com>
2024-05-06 18:42:37 +0900

    getRandomRequestState 에러 수정

--- mock/lib.ts
+++ mock/lib.ts
@@ -1,5 +1,9 @@
+import { getRandom } from "@/util/math";
+
 export type RequestState = "approved" | "rejected" | "pending";
 
 export function getRandomRequestState() {
-  return ["approved", "rejected", "pending"][Math.random() * 2] as RequestState;
+  return ["approved", "rejected", "pending"][
+    getRandom({ max: 2 })
+  ] as RequestState;
 }
